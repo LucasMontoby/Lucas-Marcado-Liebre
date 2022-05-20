918b9379c09f26d4e1cd85365797df4cf9dfa2d0
Lucas <montobybianchi@gmail.com>
2022-05-20 11:55:29 -0300

    Mercado Liebre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+app.listen(3000, () => console.log("El servidor esta corriendo en puerto 3000"))
+
+app.use(express.static(path.join(__dirname, "./public")))
+
+app.get ("/", (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/home.html"))
+})
+
+app.get ("/register", (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+app.get ("/login", (req,res) => {
+    res.sendFile(path.join(__dirname, "./views/login.html"))
+})
+
